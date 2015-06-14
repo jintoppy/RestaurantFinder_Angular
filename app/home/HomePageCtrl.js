4b06a233bc9e77051adb1c9ca8a7a603a1a4b72a
Jinto Jose <jintoppy@gmail.com>
2015-06-14 10:03:14 +0530

    Adding search boxes

---
+++ app/home/HomePageCtrl.js
@@ -0,0 +1,30 @@
+angular.module('myapp')
+	.controller('HomePageCtrl', ['$scope',
+		function ($scope) {
+			$scope.test = "Welcome to restaurant finder";
+			$scope.cities = [
+			{
+				name: 'Bangalore',
+				value: 'BNG'
+			},
+			{
+				name: 'Mumbai',
+				value: 'MUM'
+			}];
+
+			$scope.localities = [
+			{
+				name: 'IndiraNagar',
+				value: 'ING'
+			},
+			{
+				name: 'Whitefield',
+				value: 'WHF'
+			}];
+
+			$scope.onSearch = function(){
+				console.log($scope.city);
+				console.log($scope.locality);
+			};
+
+		}]);
